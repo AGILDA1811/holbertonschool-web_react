7c4dd8627a63113676feeefbfdfe5f8e945dff8e
Agilda Kulli <agildakulli@gmail.com>
2026-03-12 20:55:47 +0100

    Strengthen task 3 notifications spec

--- react_intro/task_3/dashboard/src/Notifications.spec.js
+++ react_intro/task_3/dashboard/src/Notifications.spec.js
@@ -1,7 +1,32 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import Notifications from "./Notifications";
+import Notifications from "./Notifications.jsx";
 
 describe("Notifications component", () => {
+  test("renders all required notifications elements ignoring case", () => {
+    render(<Notifications />);
+
+    const notificationTitle = screen.getByText(
+      /here is the list of notifications/i
+    );
+    const closeButton = screen.getByRole("button", { name: /close/i });
+    const closeIcon = screen.getByRole("img", { name: /close icon/i });
+    const notificationList = screen.getByRole("list");
+    const listItems = screen.getAllByRole("listitem");
+
+    expect(notificationTitle).toBeInTheDocument();
+    expect(closeButton).toBeInTheDocument();
+    expect(closeIcon).toBeInTheDocument();
+    expect(notificationList).toBeInTheDocument();
+    expect(listItems).toHaveLength(3);
+    expect(screen.getByText(/new course available/i)).toBeInTheDocument();
+    expect(screen.getByText(/new resume available/i)).toBeInTheDocument();
+    expect(screen.getByText(/urgent requirement/i)).toBeInTheDocument();
+    expect(screen.getByText(/complete by eod/i)).toBeInTheDocument();
+    expect(listItems[0]).toHaveAttribute("data-priority", "default");
+    expect(listItems[1]).toHaveAttribute("data-priority", "urgent");
+    expect(listItems[2]).toHaveAttribute("data-priority", "urgent");
+  });
+
   test("renders the notifications title", () => {
     render(<Notifications />);
 
