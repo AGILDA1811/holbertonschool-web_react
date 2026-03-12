721177626f71373a618121e0b08d99f520540dd5
Agilda Kulli <agildakulli@gmail.com>
2026-03-12 20:42:48 +0100

    Match task 3 notifications test requirements

--- react_intro/task_3/dashboard/src/Notifications.spec.js
+++ react_intro/task_3/dashboard/src/Notifications.spec.js
@@ -1,27 +1,36 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import Notifications from "./Notifications.jsx";
+import Notifications from "./Notifications";
 
 describe("Notifications component", () => {
-  test("renders the notifications list and handles the close button", () => {
-    const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
-
+  test("renders the notifications title", () => {
     render(<Notifications />);
 
-    expect(
-      screen.getByText(/here is the list of notifications/i)
-    ).toBeInTheDocument();
+    const notificationTitle = screen.getByText(
+      /here is the list of notifications/i
+    );
+    expect(notificationTitle).toBeInTheDocument();
+  });
+
+  test("renders the close button", () => {
+    render(<Notifications />);
 
     const closeButton = screen.getByRole("button", { name: /close/i });
     expect(closeButton).toBeInTheDocument();
+  });
+
+  test("renders 3 list items", () => {
+    render(<Notifications />);
 
     const listItems = screen.getAllByRole("listitem");
     expect(listItems).toHaveLength(3);
-    expect(listItems[0]).toHaveTextContent(/new course available/i);
-    expect(listItems[1]).toHaveTextContent(/new resume available/i);
-    expect(listItems[2]).toHaveTextContent(/urgent requirement/i);
-    expect(listItems[2]).toHaveTextContent(/complete by eod/i);
+  });
 
-    fireEvent.click(closeButton);
+  test("logs when the close button is clicked", () => {
+    const consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+
+    render(<Notifications />);
+    fireEvent.click(screen.getByRole("button", { name: /close/i }));
+
     expect(consoleSpy).toHaveBeenCalledWith("Close button has been clicked");
 
     consoleSpy.mockRestore();
